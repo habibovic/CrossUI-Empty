86868ec8835301a3581320a9f28f518779c00c86
Amir Habibovic <30302493+habibovic@users.noreply.github.com>
2022-10-30 21:33:14 +0100

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -24,9 +24,10 @@ xui.Class('App', 'xui.Module',{
                 xui.create("xui.UI.Button")
                 .setHost(host,"sabah_button")
                 .setName("sabah_button")
-                .setLeft("2.2857142857142856em")
-                .setTop("34.285714285714285em")
-                .setWidth("6.019047619047619em")
+                .setLeft("0.7619047619047619em")
+                .setTop("35.80952380952381em")
+                .setWidth("6.780952380952381em")
+                .setZIndex(2)
                 .setCaption("Sabah")
                 .onContextmenu([
                     {
@@ -44,7 +45,7 @@ xui.Class('App', 'xui.Module',{
                 xui.create("xui.UI.Label")
                 .setHost(host,"xui_ui_label18")
                 .setLeft("19.375em")
-                .setTop("3.125em")
+                .setTop("1.25em")
                 .setWidth("10.81904761904762em")
                 .setHeight("1.980952380952381em")
                 .setCaption("Ovo je testna aplikacija")
@@ -54,6 +55,100 @@ xui.Class('App', 'xui.Module',{
                         "font-style" : "oblique"
                     }
                 })
+            );
+            
+            append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image7")
+                .setLeft("0em")
+                .setTop("6.857142857142857em")
+                .setWidth("61.714285714285715em")
+                .setHeight("32.22857142857143em")
+                .setSrc("https://upload.wikimedia.org/wikipedia/commons/5/55/Blaue_moschee_6minarette.jpg")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button15")
+                .setName("podne_button")
+                .setLeft("8.380952380952381em")
+                .setTop("35.80952380952381em")
+                .setWidth("6.780952380952381em")
+                .setZIndex(2)
+                .setCaption("Podne")
+                .onContextmenu([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "control",
+                        "target" : "sabah_button",
+                        "args" : [ ],
+                        "method" : "fireClickEvent",
+                        "event" : 1
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button20")
+                .setName("ikindija_button")
+                .setLeft("16em")
+                .setTop("35.80952380952381em")
+                .setWidth("6.780952380952381em")
+                .setZIndex(2)
+                .setCaption("Ikindija")
+                .onContextmenu([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "control",
+                        "target" : "sabah_button",
+                        "args" : [ ],
+                        "method" : "fireClickEvent",
+                        "event" : 1
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button21")
+                .setName("aksam_button")
+                .setLeft("23.61904761904762em")
+                .setTop("35.80952380952381em")
+                .setWidth("6.780952380952381em")
+                .setZIndex(2)
+                .setCaption("Aksam")
+                .onContextmenu([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "control",
+                        "target" : "sabah_button",
+                        "args" : [ ],
+                        "method" : "fireClickEvent",
+                        "event" : 1
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button22")
+                .setName("jacija_button")
+                .setLeft("31.238095238095237em")
+                .setTop("35.80952380952381em")
+                .setWidth("6.780952380952381em")
+                .setZIndex(2)
+                .setCaption("Jacija")
+                .onContextmenu([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "control",
+                        "target" : "sabah_button",
+                        "args" : [ ],
+                        "method" : "fireClickEvent",
+                        "event" : 1
+                    }
+                ])
             );
             
             return children;
